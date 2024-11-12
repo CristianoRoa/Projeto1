b87fd83287f066999373752446450e4a67fd30f3
CristianoRoa <financeirodosanjos@gmail.com>
2024-11-11 21:47:41 -0300

    Att1

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function verificarIdade(nome, idade) {
+    
+if (idade >=18) {
+    console.log(`${nome} é maior de idade`);
+    
+} else {
+    console.log(`${nome} é menor de idade`);
+    
+}
+}
+verificarIdade("mari", 24)
+verificarIdade("João Batista", 57)
+verificarIdade("Bernardo", 6)
+verificarIdade("Cristiano", 30)
